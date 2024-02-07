614be932d3b977d6b6dc7eea7bfd19ac89d58d32
taeyuls <startez03@gmail.com>
2024-02-07 11:40:31 +0900

    slider

--- src/components/MovieModal/Row.jsx
+++ src/components/MovieModal/Row.jsx
@@ -1,6 +1,6 @@
-import './Row.css';
-import axios from '../../api/axios';
-import { useCallback, useEffect, useState } from 'react';
+import "./Row.css";
+import axios from "../../api/axios";
+import { useCallback, useEffect, useState } from "react";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
@@ -17,30 +17,43 @@ const Row = ({ title, id, fetchUrl }) => {
   return (
     <div>
       <h2>{title}</h2>
-      <div className='slider'>
-        <div className='slider__arrow-left'>
-          <span className='arrow'>
-            {"<"} 
-            </span>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
-        <div id={id} className='row__posters'>
-          {movies.map((movie) =>(
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
             <img
-            key={movie.id}
-            className='row__poster'
-            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-            alt={movie.name}
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.name}
             />
           ))}
         </div>
-        <div className='slider__arrow-right'>
-          <span className='arrow'>
-            {">"} 
-            </span>
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
         </div>
       </div>
+      {modalOpen ? (
+        <movieModal {...movieSelected} setModalOpen={setModalOpen} />
+      ) : null}
     </div>
   );
-}
+};
 
 export default Row;
